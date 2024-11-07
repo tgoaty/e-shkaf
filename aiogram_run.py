4731cc02c40a3a35e8edae0083b7cdd0e84aa2d7
Nikolay Pushkin <adwedelf@gmail.com>
2024-11-07 10:44:40 +0700

    fix: aiogram_start & order handler

--- aiogram_run.py
+++ aiogram_run.py
@@ -1,54 +1,13 @@
-import logging
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
-from os import getenv
+from create_bot import bot, dp, scheduler
+from handlers import menu_router, order_router, orderList_router, profile_router, request_router, start_router
 
-from sqlalchemy.util import await_fallback
-from db_handler import db_class  # Импортируем наш класс базы данных
-from dotenv import load_dotenv
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-# Загружаем переменные окружения из .env
-load_dotenv()
-
-# Планировщик задач
-scheduler = AsyncIOScheduler(timezone='Asia/Barnaul')
-
-# Извлечение списка администраторов из .env
-admins = [int(admin_id) for admin_id in getenv('ADMINS').split(',')]
-
-# Настройка логгера
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-# Инициализация бота
-bot = Bot(token=getenv('TELEGRAM_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-# Хранение данных бота в памяти
-dp = Dispatcher(storage=MemoryStorage())
-
-# Инициализируем объект базы данных
-db = db_class.Database()
-
-
-# Логгирование подключения к бд
-# TODO причесать файл
-async def on_startup():
-    try:
-        await db.connect()
-        logger.info("Бот успешно подключен к базе данных.")
-    except Exception as e:
-        logger.error(f"Ошибка подключения к базе данных: {e}")
-        await bot.close()
-        raise e
-
-    await db.disconnect()
 
 async def main():
-    await on_startup()
+    dp.include_routers( menu_router, orderList_router, order_router, profile_router, request_router, start_router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- create_bot.py
+++ create_bot.py
@@ -1,30 +1,53 @@
 import logging
+import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from os import getenv
 
-from db_handler import db_class
-
+from db_handler import db_class  # Импортируем наш класс базы данных
 from dotenv import load_dotenv
-
-load_dotenv()
-
-db = db_class.Database()
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+# Загружаем переменные окружения из .env
+load_dotenv()
+
+# Планировщик задач
 scheduler = AsyncIOScheduler(timezone='Asia/Barnaul')
 
-# извлечение админов из .env
+# Извлечение списка администраторов из .env
 admins = [int(admin_id) for admin_id in getenv('ADMINS').split(',')]
 
-# создание логгера
+# Настройка логгера
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+# Инициализация бота
 bot = Bot(token=getenv('TELEGRAM_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-# храним данные бота в RAM
+# Хранение данных бота в памяти
 dp = Dispatcher(storage=MemoryStorage())
+
+# Инициализируем объект базы данных
+db = db_class.Database()
+
+
+# Логгирование подключения к бд
+# TODO причесать файл
+async def on_startup():
+    try:
+        await db.connect()
+        logger.info("Бот успешно подключен к базе данных.")
+    except Exception as e:
+        logger.error(f"Ошибка подключения к базе данных: {e}")
+        await bot.close()
+        raise e
+
+    await db.disconnect()
+
+async def main():
+    await on_startup()
+
+if __name__ == "__main__":
+    asyncio.run(main())
